85f9c44fd4cffdd26be501b3f0b94e6f442138c3
cgung7 <cgung7@naver.com>
2025-10-22 12:13:31 +0900

    20251022JS local

--- c_javascript/r_저장소/d_local.js
+++ c_javascript/r_저장소/d_local.js
@@ -8,3 +8,60 @@
 //! 로컬 스토리지 데이터 저장
 // localStorage.setItem(key, value);
 // - 웹 브라우저의 저장공간에 데이터 저장 (브라우저를 닫아도 유지)
+
+localStorage.setItem('key', 'value');
+localStorage.setItem('username', '최광섭');
+
+// cf) 로컬 스토리지의 데이터는 문자열만 저장 가능
+// : 기본 자료형 또한 저장 가능 >> 문자열로 인식
+localStorage.setItem('number', 35);
+localStorage.setItem('boolean', false);
+
+// cf) 로컬 스토리지에 객체나 배열 등의 참조 자료형 데이터 저장 시
+//      : 문자열 형태로 변환 (JSON.stringfy())
+let userInfo = {
+  name: '최광섭',
+  age: 35
+}
+
+localStorage.setItem('userInfo', JSON.stringify(userInfo));
+
+//! 로컬 스토리지 데이터 가져오기
+// localStorage.getItem(key);
+// : 해당 key와 일치한 데이터 반환, 없으면 null 반환
+
+// 1) 기본 자료형
+let keyValue = localStorage.getItem('key');
+let username = localStorage.getItem('username');
+let numberValue = localStorage.getItem('number');
+let booleanValue = localStorage.getItem('boolean');
+
+console.log(keyValue);
+console.log(username);
+console.log(numberValue);
+console.log(booleanValue);
+
+// 2) 참조 자료형
+let userInfoData = localStorage.getItem('userInfo');
+console.log(userInfoData); // {"name":"최광섭","age":"35"}
+console.log(JSON.parse(userInfoData)); // {name: '최광섭', age: 35}
+
+//? cf) 로컬 스토리지 데이터 반환 시 데이터의 null 값 처리 코드 작성 필수
+let noValue = localStorage.getItem('no');
+console.log(noValue); // null
+
+// noValue.toLowerCase();
+if (noValue) {
+  noValue.toUpperCase();
+}
+
+//! 로컬 스토리지 데이터 삭제
+// : localStorage.removeItem(key);
+// - 초기화 localStorage.clear();
+
+localStorage.removeItem('username');
+console.log(localStorage.getItem('username')); // null
+
+localStorage.removeItem('hello');
+
+localStorage.clear();

2653a2e2f8c975c64b5474e33910bc69728c0ba2
cgung7 <cgung7@naver.com>
2025-11-05 11:51:16 +0900

    20251105React Custom

---
+++ f-iot-react/src/_practice/SearchApp.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+//! 웹사이트 검색 기능
+
+// 폴더 구조
+//@ _practice /
+//      _SearchApp.tsx
+// hooks /
+//      useState.ts
+// components /
+//    SearchBox.tsx
+//    SearchResults.tsx
+
+function SearchApp() {
+  return (
+    <div>SearchApp</div>
+  )
+}
+
+export default SearchApp
--- f-iot-react/src/pages/c_hooks/f_custom/Custom02.tsx
+++ f-iot-react/src/pages/c_hooks/f_custom/Custom02.tsx
@@ -23,20 +23,42 @@ function Custom02() {
     bind: nameBind 
   } = example01;
 
+  //? 구조 분해 할당 + 커스컴 훅 사용
   const {
     value: email,
     handleReset: emailReset,
     bind: emailBind
   } = useInput('');
+  
+  //@ 구조 분해 할당 
+  const person = { nickname: '최광섭', age: 35 };
+  const { nickname, age } = person;
+  console.log(nickname);
+  console.log(age);
 
+  //^ 구조 분해 할당 시 가져오는 데이터의 명칭(키) 변경
+  // const {실제속성값: 변경할명칭} = 분해할데이터'
+  const { nickname: nn } = person;
+  console.log(nn);
+
+  const handleAllReset = () => {
+    nameReset();
+    emailReset();
+  }
 
   return (
     <div>
       <p>Name: {name}</p>
       <input type="text" name='name' placeholder='사용자 이름' {...nameBind}/>
+      <button onClick={nameReset}>이름 초기화</button>
 
       <p>Email: {email}</p>
       <input type="text" name='email' placeholder='사용자 이메일' {...emailBind}/>
+      <button onClick={emailReset}>이메일 초기화</button>
+      
+      <br />
+
+      <button onClick={handleAllReset}>전체 초기화</button>
     </div>
   )
 }
